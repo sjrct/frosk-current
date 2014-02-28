e2865d10e23fb48d8414ef7ce90b6725c39ad61d
Chris Harding <hardinc@clarkson.edu>
2014-02-28 16:27:11 -0500

    Fixed PCI code

--- kernel/src/pci.c
+++ kernel/src/pci.c
@@ -6,35 +6,36 @@
 #include "pci.h"
 #include "debug.h"
 
-#define PCI_ADDR_PORT 0xCF8
-#define PCI_DATA_PORT 0xCFC
+#define CONFIG_ADDR 0xCF8
+#define CONFIG_DATA 0xCFC
 
 dword pci_addr(byte bus, byte dev, byte fun, byte off)
 {
 	return (1 << 31)
-		|  ((dword)off & 0xfc)
+		|  ((dword)off & 0xFC)
 		| (((dword)fun & 0x07) <<  8)
-		| (((dword)dev & 0x1f) << 11)
-		| (((dword)bus & 0xff) << 16);
+		| (((dword)dev & 0x1F) << 11)
+		|  ((dword)bus         << 16);
 }
 
 dword pci_read(dword addr)
 {
-	outl(PCI_ADDR_PORT, addr);
-	return inl(PCI_DATA_PORT);
+	outl(CONFIG_ADDR, addr);
+	return inl(CONFIG_DATA);
 }
 
 void pci_detect(void)
 {
-	dword id;
-	byte bus, dev;
+	dword id, cls;
+	dword bus, dev;
 
 	for (bus = 0; bus < 256; bus++) {
 		for (dev = 0; dev < 32; dev++) {
-			id = pci_addr(bus, dev, 0, 0);
+			id = pci_read(pci_addr(bus, dev, 0, 0));
+			cls = pci_read(pci_addr(bus, dev, 0, 8)); 
 
 			if ((id & 0xffff) != 0xffff) {
-				dprintf("Found device: %x\n", id);
+				dprintf("Found device %d on bus %d: %x | %x\n", dev, bus, id, cls);
 			}
 		}
 	}

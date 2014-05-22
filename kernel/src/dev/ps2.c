88ebd51f78fc596c955824a6a6eb83d7ecf2718f
Chris Harding <hardinc@clarkson.edu>
2014-05-21 23:05:09 -0500

    Basic PS/2 keyboard driver in working order

--- kernel/src/dev/ps2.c
+++ kernel/src/dev/ps2.c
@@ -7,26 +7,81 @@
 #include "../lock.h"
 #include "../debug.h"
 #include "../interrupt.h"
+#include "../scheduler.h"
+#include "../memory/kernel.h"
 
 #define KB_BUFFER_SIZE 1024
 
 byte kb_buffer[KB_BUFFER_SIZE];
 fs_entry_t * kb_buffer_file = NULL;
 
-void kb_put(byte sc)
+typedef struct ls {
+	struct ls * next;
+	byte sc;
+} ls_t;
+
+static ls_t * first = NULL;
+static ls_t * last  = NULL;
+static lock_t ls_lock = UNLOCKED;
+static process_t * delay_proc = NULL;
+
+static int kb_put_actual(byte sc)
 {
 	static qword kb_count = 0;
-	dprintf("sc = %d\n", sc);
 
-	assert(kb_buffer_file != NULL);
-	getlock(&kb_buffer_file->lock);
-	dputs("fdsa\n");
+	if (!trylock(&kb_buffer_file->lock)) return 0;
 
 	kb_buffer[kb_count % KB_BUFFER_SIZE] = sc;
 	kb_buffer_file->u.file.size = ++kb_count;
 
 	unlock(&kb_buffer_file->lock);
-	dputs("fdsa\n");
+
+	return 1;
+}
+
+static void kb_put_delayed(void)
+{
+	ls_t * l;
+
+	for (;;) {
+		if (first != NULL) {
+			getlock(&ls_lock);
+
+			l = first;
+			first = l->next;
+			if (first == NULL) last = NULL;
+
+			unlock(&ls_lock);
+
+			while (!kb_put_actual(l->sc));
+			kfree(l);
+		}
+	}
+}
+
+void kb_put(byte sc)
+{
+	ls_t * l;
+	assert(kb_buffer_file != NULL);
+
+	if (!kb_put_actual(sc)) {
+		l = kalloc(sizeof(ls_t));
+		l->next = NULL;
+		l->sc = sc;
+
+		getlock(&ls_lock);
+		if (first == NULL) {
+			first = last = l;
+		} else {
+			last->next = l;
+			last = l;
+		}
+		unlock(&ls_lock);
+
+		if (delay_proc == NULL) {
+			delay_proc = kspawn(kb_put_delayed);
+		}
+	}
 }
 
 qword kb_fs_read(byte * to, qword addr, qword size, void * unused)
--- prgm/start/src/start.c
+++ prgm/start/src/start.c
@@ -4,11 +4,9 @@ int main() {
 	file_t inp;
 	file_t f = fs_aquire("/dev/tvb", 0);
 
-
 	for (;;) {
 		inp = fs_aquire("/dev/kb", 0);
-
-		if (fs_size(inp) > 6) {
+		if (inp && fs_size(inp) > 6) {
 			fs_write((byte *)"asdfkasdffasdfas", 0, 10, f);
 		}
 		fs_release(inp);

cc3c174068d9b155562698123fd57aff133a9151
Chris Harding <sjrct@users.noreply.github.com>
2024-11-26 23:43:38 -0500

    kernel: fix a paging bug
    The user space was getting allocated inside unused space defaultly
    allocated to the kernel, which was causing an unexpected page protection
    fault when trying to grow the heap.

--- kernel/src/paging.c
+++ kernel/src/paging.c
@@ -20,8 +20,21 @@
 #define ERROR_HLAT         0x80 // Caused by HLAT paging issue
 #define ERROR_SGX        0x8000 // Caused by Software Guard Extension
 
-#define PG_SWP_ENT (PM_L1_LOC + PG_SWP_LOC / 0x200)
+// The location in P1 that maps PG_SWP
+#define PG_SWP_ENT (PM_L1_LOC + PG_SWP_LOC / PAGE_SIZE * sizeof(ulong))
 
+/*
+ * Gets an entry in a page map table for an address. Every page map table is a
+ * page (0x1000 bytes) of entries that maps a particular portion of the address
+ * to either another, lower level page map or to the address itself (at the
+ * lowest, the "page table" level)
+ *
+ * Parameters:
+ *   addr - the address to lookup the entry for
+ *   tbl  - the address of the table to lookup the entry in
+ *   shf  - numbers of bits to shift addr to get the base of the address we are
+ *          trying to look up.
+ */
 static ulong get_entry(ulong addr, ulong tbl, int shf)
 {
 #ifdef __ARCH_X86_64
@@ -35,6 +48,10 @@ static ulong get_entry(ulong addr, ulong tbl, int shf)
 #undef LSHIFT
 }
 
+/**
+ * Page in the page map for the particular addr/tbl/shift into the page swap
+ * page (PG_SWP_LOC), allocating a new page if necessary.
+ */
 static void down(ulong addr, ulong tbl, int shift)
 {
     long i;
@@ -50,34 +67,16 @@ static void down(ulong addr, ulong tbl, int shift)
 
         for (i = 0; i < PAGE_SIZE; i++) atb(PG_SWP_LOC + i) = 0;
     } else {
+        // TODO maybe checking if this is already true an not invlpging woulg
+        //      be an optimization
         atq(PG_SWP_ENT) = v;
         invlpg(PG_SWP_LOC);
     }
 }
 
-void pageto(ulong virt, ulong value)
-{
-    assert(!(virt % PAGE_SIZE));
-    invlpg(virt);
-
-#ifdef __ARCH_X86_64
-    if (virt >= HIGH_HALF_BOT) {
-        virt -= NON_CANON_SIZE;
-    }
-
-    down(virt, PM_L4_LOC,  39);
-    down(virt, PG_SWP_LOC, 30);
-    down(virt, PG_SWP_LOC, 21);
-#else
-    down(virt, PM_L2_LOC, 22);
-#endif
-    atq(get_entry(virt, PG_SWP_LOC, 12)) = value;
-}
-
 ulong getpage(ulong virt)
 {
     assert(!(virt % PAGE_SIZE));
-    invlpg(virt);
 
 #ifdef __ARCH_X86_64
     if (virt >= HIGH_HALF_BOT) {
@@ -93,7 +92,14 @@ ulong getpage(ulong virt)
     return get_entry(virt, PG_SWP_LOC, 12);
 }
 
-void print_region(const char *str, region_t *rgn) {
+void pageto(ulong virt, ulong value)
+{
+    invlpg(virt);
+    atq(getpage(virt)) = value;
+}
+
+void print_region(const char *str, region_t *rgn)
+{
     dprintf("rgn %s(%d) : %X (%X)\n", str, rgn->growup, rgn->virt, rgn->size);
 
     block_t *b = rgn->first;
@@ -108,6 +114,8 @@ void page_fault(ulong error, ulong cr2, ulong instr_ptr)
     region_t * heap;
     region_t * stack;
     ulong page;
+
+    dprintf("page_fault(error=%X, cr=%X, instr_ptr=%X)\n", error, cr2, instr_ptr);
 
     if (error == (ERROR_WRITE | ERROR_USER)) {
         /* Userspace, write, page-not-present error */
@@ -131,8 +139,15 @@ void page_fault(ulong error, ulong cr2, ulong instr_ptr)
             }
         }
     } else {
-        dprintf("Unhandlable page fault: error=%X addr=%p", error, cr2);
-        dprintf("At instruction %p\n", instr_ptr);
+        if ((error & ERROR_USER) && (error & ERROR_PROTECTION)) {
+            // TODO kill thread/process
+            dprintf("Protection fault (user tried to access restricted memory)\n");
+        }
+
+        dprintf("Unhandlable page fault: error=%X addr=%p\n", error, cr2);
+        dprintf("  At instruction %p\n", instr_ptr);
+        dprintf("  page(%p) = %p\n", cr2, getpage(cr2));
+
         for(;;);
     }
 }
